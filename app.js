b19f546568ee9120e2b433cd45f4b1eddbf2aa69
safabeegum <pasafabeegum@gmail.com>
2024-06-10 22:46:39 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,31 @@
+const express= require("express")
+const mongoose= require("mongoose")
+const cors= require("cors")
+
+const app=express()
+app.use(cors())
+
+app.get("/",(req,res) => {
+    res.send("Hello")
+})
+
+app.post("/add",(req,res) => {
+    res.send("Welcome to my Add Page")
+})
+
+
+app.post("/search",(req,res) => {
+    res.send("Search Page")
+})
+
+app.post("/delete",(req,res) => {
+    res.send("Delete Page")
+})
+
+app.get("/view",(req,res) => {
+    res.send("View Page")
+})
+
+app.listen(8080,() => {
+    console.log("server started")
+})
